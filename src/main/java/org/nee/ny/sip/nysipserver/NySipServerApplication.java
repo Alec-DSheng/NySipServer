d5f8327508cd05d5bf364630a7a7567aa1154b63
huangwanming <huangwanming@enn.cn>
2020-11-27 11:10:46 +0800

    first commit

---
+++ src/main/java/org/nee/ny/sip/nysipserver/NySipServerApplication.java
@@ -0,0 +1,13 @@
+package org.nee.ny.sip.nysipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NySipServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NySipServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nee/ny/sip/nysipserver/NySipServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nee.ny.sip.nysipserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NySipServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
