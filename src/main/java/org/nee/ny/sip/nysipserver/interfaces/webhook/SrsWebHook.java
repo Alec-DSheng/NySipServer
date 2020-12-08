33a852c05ee77f8dee45465d301c56786423a971
Alec-DSheng <qlzw226@sina.com>
2020-12-08 22:38:17 +0800

    对接zlmedia

---
+++ src/main/java/org/nee/ny/sip/nysipserver/interfaces/webhook/SrsWebHook.java
@@ -0,0 +1,14 @@
+package org.nee.ny.sip.nysipserver.interfaces.webhook;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: alec
+ * Description: 监听SRS WebHook
+ * @date: 22:29 2020-12-08
+ */
+@RestController
+@RequestMapping(value = "srs")
+public class SrsWebHook {
+}
