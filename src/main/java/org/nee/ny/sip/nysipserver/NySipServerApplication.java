d180411c1328298b1dc0be86f90eaa196be2a528
Alec-DSheng <qlzw226@sina.com>
2020-12-15 18:16:00 +0800

    web hook

--- src/main/java/org/nee/ny/sip/nysipserver/NySipServerApplication.java
+++ src/main/java/org/nee/ny/sip/nysipserver/NySipServerApplication.java
@@ -1,15 +1,18 @@
 package org.nee.ny.sip.nysipserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Slf4j
 public class NySipServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NySipServerApplication.class, args);
+        log.info("~~~~~~~~~~~SIP 信令调度服务器启动~~~~~~~~~~~~~~~~");
     }
 
 }
